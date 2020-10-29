01dace703af56ff97b0a72d1cf7e6f185a8d4e1f
Lubomir Molcan <lmolcan@globalblue.com>
2020-10-29 09:42:49 +0100

    added headr and responsive design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,15 @@ import { LoginComponent } from './login/login.component';
 import { ContentComponent } from './content/content.component';
 import { DocsComponent } from './docs/docs.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatMenuModule } from '@angular/material/menu';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +27,16 @@ import { DocsComponent } from './docs/docs.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
