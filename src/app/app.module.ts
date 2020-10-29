c371ec8fb26da3d10f613a69e00c28e40cbcb81e
Lubomir Molcan <lmolcan@globalblue.com>
2020-10-29 09:06:03 +0100

    added 4 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { ContentComponent } from './content/content.component';
+import { DocsComponent } from './docs/docs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    ContentComponent,
+    DocsComponent
   ],
   imports: [
     BrowserModule,
